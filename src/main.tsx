0de8803c75d2a25a731c7e55de61460d9a79df16
imran1khan <107169527+imran1khan@users.noreply.github.com>
2024-06-09 09:26:42 +0530

    new up005

--- src/main.tsx
+++ src/main.tsx
@@ -4,45 +4,46 @@ import App from './App.tsx'
 import './index.css'
 import { RecoilRoot } from 'recoil'
 import LandingPage from './Pages/LandingPage.tsx'
-import { KindeProvider } from "@kinde-oss/kinde-auth-react";
+// import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 import Dashboard from './Pages/Dashboard.tsx'
 import { Toaster } from 'react-hot-toast'
 import {  Routes, Route, BrowserRouter } from 'react-router-dom'
 
-const onRedirectCallback = (user: any, app_state: any) => {
-  console.log({ user, app_state });
-  // Redirect to a dashboard or home page
-  const sendFile = async () => {
-    try {
-      const response = await fetch('/api/adduser', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          userId: user?.id || '',
-          userEmail: user?.email || ''
-        })
-      });
-      console.log(await response.json());
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  sendFile();
-  window.location.href = '/dashboard';
-};
+// const onRedirectCallback = (user: any, app_state: any) => {
+//   console.log({ user, app_state });
+//   // Redirect to a dashboard or home page
+//   const sendFile = async () => {
+//     try {
+//       const response = await fetch('/api/adduser', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//           userId: user?.id || '',
+//           userEmail: user?.email || ''
+//         })
+//       });
+//       console.log(await response.json());
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   }
+//   sendFile();
+//   window.location.href = '/dashboard';
+// };
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RecoilRoot>
-      <KindeProvider
+      {/* <KindeProvider
         clientId="9932afa04b9241b3bf005becd78c82be"
         domain="https://exceladrawclone.kinde.com"
         redirectUri="https://eraser-dev-clone.vercel.app/"
         logoutUri="https://eraser-dev-clone.vercel.app/"
         onRedirectCallback={onRedirectCallback}
       >
+      </KindeProvider> */}
         <BrowserRouter>
           <Routes>
             <Route path='/' element={<LandingPage />} />
@@ -51,7 +52,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           </Routes>
         </BrowserRouter>
         <Toaster />
-      </KindeProvider>
     </RecoilRoot>
   </React.StrictMode>,
 )
